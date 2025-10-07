dbd5ca88b868f7da88c365b3c2764a94be87b943
Karan Kumar <thekarankumar.dev@gmail.com>
2025-10-07 16:16:43 +0530

    complete-product-card

--- Prodify/Views/Cells/ProductTableViewCell.swift
+++ Prodify/Views/Cells/ProductTableViewCell.swift
@@ -9,6 +9,51 @@ import UIKit
 
 class ProductTableViewCell: UITableViewCell {
 
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UIButton!
+    
+    private var imageURL: URL?
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        accessoryType = .disclosureIndicator
+        
+    }
+    required init?(coder: NSCoder) { fatalError() }
+
+    
+    func configure(with product: Product) {
+            titleLabel.text = product.title
+            descLabel.text = product.description
+            categoryLabel.setTitle(product.category, for: .normal)
+            priceLabel.text = "₹\(product.price)"
+            productImageView.image = nil
+
+            if let url = product.image {
+                imageURL = url
+                ImageLoader.shared.loadImage(from: url) { [weak self] img in
+                    guard let self = self else { return }
+                    if self.imageURL == url { self.productImageView.image = img }
+                }
+            } else {
+                imageURL = nil
+                productImageView.image = nil
+            }
+        }
+
+        override func prepareForReuse() {
+            super.prepareForReuse()
+            if let url = imageURL { ImageLoader.shared.cancelLoading(url: url) }
+            imageURL = nil
+            productImageView.image = nil
+        }
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
